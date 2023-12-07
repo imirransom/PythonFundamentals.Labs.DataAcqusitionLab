f07510513d295a26da473fc20b71463291861896
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-12-06 20:48:45 -0500

    got my access key to work

--- make_request.py
+++ make_request.py
@@ -1,4 +1,10 @@
 import requests
+url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/locations/'
+tokken = 'WqSwoxMEAbdGjwBOussoPXwHZibftJDh'
+# The access key needs to be included in the header and needs to have 'token' as its key
+head = {'token': tokken}
 
-response = requests.get('https://www.ncei.noaa.gov/cdo-web/api/v2/locations')
+response = requests.get(url, headers=head)
 print(response.status_code)
+
+print(response.json())
